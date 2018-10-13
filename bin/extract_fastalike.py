0179a186bfc948bccbad5208a26bb106937a0ad2
Darcy Jones <darcy.ab.jones@gmail.com>
2018-10-13 14:10:32 +0800

    wrote script to extract fastalike files

---
+++ bin/extract_fastalike.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def check_last_was(block):
+    if len(block) == 0:
+        return False
+    else:
+        return block[-1].startswith(">")
+
+
+def get_block_filename(block):
+    header = block[0]
+    basename = header.lstrip("> ").split(" ", 1)[0]
+    return basename + ".fasta"
+
+
+def main():
+
+    if len(sys.argv) != 2:
+        print("Expected a file to process")
+        sys.exit(1)
+
+    fastalike = sys.argv[1]
+
+    current = []
+    with open(fastalike, "r") as handle:
+        _ = next(handle)  # Skip first line
+        for line in handle:
+            if line.startswith(">") and check_last_was(current):
+                assert len(current) > 1
+
+                filename = get_block_filename(current)
+                with open(filename, "w") as out_handle:
+                    print("".join(current[:-1]), file=out_handle)
+                current = []
+
+            current.append(line)
+
+        assert len(current) > 1
+
+        filename = get_block_filename(current)
+        with open(filename, "w") as out_handle:
+            print("".join(current[:-1]), file=out_handle)
+
+
+if __name__ == "__main__":
+    main()
